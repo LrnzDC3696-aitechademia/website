8ed70360e7b2d9a4e368bf3c72e8d9420dda602b
WeMayNeverKnow <77531972+WeMayNeverKnow@users.noreply.github.com>
2021-12-04 23:49:32 +0800

    templates

--- src/app.py
+++ src/app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+#  from .forms import RegistrationForms, LoginForms
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a very secret key that you don\'t know'
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#  @app.route('/register/')
+#  def register():
+#      form = RegistrationForms()
+#      return render_template('register.html', title='Register', form=form)
+#
+#  @app.route('/login/')
+#  def login():
+#      form = LoginForms()
+#      return render_template('login.html', title='Login', form=form)
+
 
