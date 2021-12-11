075abe541c27b1457d13261f5cd42be113353d3f
m0nk3ybraindead <zyphr.euch@gmail>
2021-12-11 18:40:33 +0800

    Corey Schafer part 7 tutorial done pog
    Added a change profile stuff
    Added a profile picture function that addes the user pfp ofc
    Also added a function that deletes the old pfp of the user to have lesser storage

--- src/forms.py
+++ src/forms.py
@@ -1,4 +1,6 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 
@@ -42,3 +44,37 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Login")
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=3, max=30)]
+    )
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField(
+        "Update Profile Picture", validators=[FileAllowed(["jpeg", "jpg", "png"])]
+    )
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data == current_user.username:
+            return
+
+        user = User.query.filter_by(username=username.data).first()
+
+        if user is None:
+            return
+
+        raise ValidationError("Username is taken. Please take another one")
+
+    def validate_email(self, email):
+        if email.data == current_user.email:
+            return
+
+        user = User.query.filter_by(email=email.data).first()
+
+        if user is None:
+            return
+
+        raise ValidationError("Email is taken. No alts allowed")
--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,12 @@
+import os
+import secrets
+
+from PIL import Image
 from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
 from src import app, bcrypt, db
-from src.forms import LoginForm, RegistrationForm
+from src.forms import LoginForm, RegistrationForm, UpdateAccountForm
 from src.models import Post, User
 
 
@@ -77,7 +81,45 @@ def logout():
     return redirect(url_for("index"))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
+
+    OUTPUT_SIZE = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(OUTPUT_SIZE)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+def delete_picture(picture_fn):
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
+    os.remove(picture_path)
+
+
+@app.route("/account/", methods=["GET", "POST"])
 @login_required
 def account():
-    return render_template("account.html", title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            delete_picture(current_user.image_file)
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for("static", filename=f"profile_pics/{ current_user.image_file }")
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
